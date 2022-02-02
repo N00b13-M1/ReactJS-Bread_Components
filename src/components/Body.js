c13b2c14d1ab08a1020303db632338fadbdac30f
N00b13-M1 <michael.cornelis@gmail.com>
2022-02-02 17:47:49 +0100

    FinalCommit

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,11 +3,37 @@ import '../css/Body.css'
 import Article from './Article';
 
 class Body extends Component {
+  constructor(props){
+    super(props)
+    this.state = {nombreArticles: "1"}
+  }
+  picker = () => {
+    const number = document.getElementById("mySelect").value
+    this.setState({nombreArticles: number})
+    console.log(typeof number)
+  }
+
   render(){
     return (
       <div className="">
-        <Article/>
-        <Article/>
+        <div className="picker d-flex justify-content-center align-itemw-center">
+          <p className="text-white my-auto">Select a number of articles:</p>
+          <select id="mySelect" className="ms-2" onChange={this.picker}>
+            <option value="1">1</option>
+            <option value="2">2</option>
+          </select>
+        </div>
+        {
+          this.state.nombreArticles === "2" &&
+          <div>
+            <Article />
+            <Article />
+          </div>
+        }
+        {
+          this.state.nombreArticles !== "2" &&
+          <h1 className="text-center text-white p-5">Il n'y a pas d'articles </h1>
+        }
       </div>
     );
   }
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,9 +2,9 @@ import React, {Component} from 'react';
 import '../css/Navigation.css'
 
 class Navigation extends Component {
-  myClick = () => (
-    console.log('Accueil')
-  )
+  // myClick = () => (
+  //   alert('Accueil')
+  // )
   // clickCounter = (e) => {
   //   switch (e.detail) {
   //     case 1:
@@ -21,12 +21,12 @@ class Navigation extends Component {
   render(){
     return (
       <div className="d-flex justify-content-evenly mx-5 my-4">
-        <input className="link" type="button" value="Link1" onClick={this.myClick} />
+        {/* <input className="link" type="button" value="Link1" onClick={this.myClick} /> */}
+        <input className="link" type="button" value="Link1" onClick={() => alert('Acceuil')} />
         <input className="link" type="button" value="Link2" 
-        onMouseEnter={() => console.log('Bienvenue à la Galerie')}
-        onMouseLeave={() => console.log('Salut')}
-         />
-        <input className="link" type="button" value="Link3" onDoubleClick={() => console.log('Contact')} />
+        onMouseOver={() => alert('Bienvenue à la Galerie')}
+        />
+        <input className="link" type="button" value="Link3" onDoubleClick={() => alert('Contact')} />
       </div>
     );
   }
